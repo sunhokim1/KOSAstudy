939b88709bcb7e6fc0e45f8b4d18224b3e687ee1
cheny <cheyeon001214@naver.com>
2025-05-01 17:15:34 +0900

    [BOJ] ATM/실버4

--- codus/week01/11399_ATM.java
+++ codus/week01/11399_ATM.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.Arrays;
  
