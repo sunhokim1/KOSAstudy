dc2c0dbeb7c5ccc2a0b553c2880da0087508ac31
yjungs2 <juuung17@gmail.com>
2025-05-05 18:36:54 +0900

    [BOJ 11399] ATM/Silver4

--- yjungs2/week01/11399_ATM.java
+++ yjungs2/week01/11399_ATM.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//BOJ 11399 ATM
+// BOJ 11399 ATM
 class BOJ11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
