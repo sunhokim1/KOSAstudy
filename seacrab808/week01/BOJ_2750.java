3d8d128e16f51da37c610b4ad077ea9d912cc5fe
소유나 <93272421+seacrab808@users.noreply.github.com>
2025-05-07 12:13:24 +0900

    helllo

--- seacrab808/week01/BOJ_2750.java
+++ seacrab808/week01/BOJ_2750.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.io.*;
 
 public class BOJ_2750 {
@@ -38,4 +39,35 @@ public class BOJ_2750 {
 			System.out.println(val);
 		}
 	}
+=======
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2750 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    int arr[] = new int[N];
+
+    for(int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    for(int i = 0; i < N - 1; i++) {
+      for(int j = 0; j < N - 1 - i; j++) {
+        if(arr[j] > arr[j + 1]) {
+          int tmp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = tmp;
+        }
+      }
+    }
+
+    for(int val : arr) {
+      System.out.println(val);
+    }
+  }
+>>>>>>> aaf27c8 ([BOJ 2750] 수 정렬하기1)
 }
