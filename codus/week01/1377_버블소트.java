9feeb32599837975673792e315484c2d5604be7c
cheny <cheyeon001214@naver.com>
2025-05-07 18:23:45 +0900

    [BOJ] 버블소트/골드

--- codus/week01/1377_버블소트.java
+++ codus/week01/1377_버블소트.java
@@ -1,1 +1,49 @@
 package codus.week01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+class Point implements Comparable<Point>{
+    int num;
+    int idx;
+
+    Point(int num, int idx){
+        this.num = num;
+        this.idx = idx;
+    }
+    @Override
+    public int compareTo(Point o) {
+        return num - o.num;
+    }
+    
+}
+
+class BubbleSort{
+   
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+        int N = Integer.parseInt(br.readLine());
+        
+        Point point[] = new Point[N];
+
+        for(int i=0; i<N; i++){
+            point[i] = new Point(Integer.parseInt(br.readLine()), i);
+        }
+        Arrays.sort(point, 0, N);
+        
+        int max = 0;
+        for(int i=0; i<N; i++){
+            max = Math.max(max, point[i].idx-i);
+        }
+        bw.write(max+1 +"\n");
+        bw.flush();
+        bw.close();
+
+    }
+}
