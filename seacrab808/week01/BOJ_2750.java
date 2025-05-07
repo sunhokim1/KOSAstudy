7d6329a295520666026e2c4cb95c3f8117d9adcb
소유나 <93272421+seacrab808@users.noreply.github.com>
2025-05-07 11:54:37 +0900

    d

---
+++ seacrab808/week01/BOJ_2750.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class BOJ_2750 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLint());
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+	}
+}
+
+public class BOJ_2750 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+
+		for (int val : arr) {
+			System.out.println(val);
+		}
+	}
+}
