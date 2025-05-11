0d05d4436d84731785cd365bb7b98ca949091781
yylee0102 <yyiww112@gmail.com>
2025-05-11 18:30:32 +0900

    [BOJ] 10989 SILVER V

---
+++ yeol/python/week02/10989_수정렬하기3.py
@@ -0,0 +1,17 @@
+
+input = sys.stdin.read
+data = list(map(int, input().split()))
+
+N = data[0]
+A = data[1:]
+
+
+
+count = [0]*10001
+for idx in A :
+    count[idx] += 1
+
+for i in range(len(count)) :
+    if count[i] != 0 :
+        for _ in range(count[i]) :
+          print(i)
