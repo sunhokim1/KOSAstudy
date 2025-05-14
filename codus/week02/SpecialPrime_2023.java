82e7bb67c70eec86ebae5a11c6af5b6a068f17c7
cheny <cheyeon001214@naver.com>
2025-05-14 11:43:52 +0900

    [BOJ] 신기한소수2023/골드5

--- codus/week02/SpecialPrime_2023.java
+++ codus/week02/SpecialPrime_2023.java
@@ -1,52 +1,62 @@
 package codus.week02;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class SpecialPrime_2023 {
+    static int N = 0;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        int arr[] = new int[N];
-        int sum = 0;
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-        }
-        Arrays.sort(arr);
-        sb.append(sum/N).append("\n");
-        
-        sb.append(arr[N/2]).append("\n");
+        N = Integer.parseInt(br.readLine());
 
-        int fre = 1;
-        int max = 1;
-        int answer = 0;
-        for(int i=N-1; i>1; i--){
-            if(arr[i-1] == arr[i]){
-                fre++;
-                if(max < fre){
-                    max= fre;
-                    answer = arr[i];
-                }
-            }else{
-                fre = 1;
+        DFS(2,1);
+        DFS(3,1);
+        DFS(5,1);
+        DFS(7,1);
+
+        bw.write(sb.toString()+"\n");
+        bw.close();
+    }
+
+    private static void DFS(int num, int step){
+        if(step == N){
+            if(isPrime(num)){
+                sb.append(num+"\n");
+                return ;
             }
         }
-        sb.append(max).append("\n");
-        sb.append(arr[N-1] - arr[0]).append("\n");
+        for(int i=1; i<=9; i+=2){
+            int next = num * 10 + i;
+            if (isPrime(next)) {
+                DFS(next, step + 1);
+            }
+        }
 
-        bw.write(sb+"\n");
-        bw.close();
+    }
 
+    private static boolean isPrime(int num){
+        if(num < 2)
+            return false;
+        if(num == 2)
+            return true;
+        if(num != 2 && num%2 == 0){
+            return false;
+        }
+        for(int i=3; i<=(int)Math.sqrt(num); i++){
+            if(num%i == 0)
+                return false;
+        }
+        return true;
     }
 }
