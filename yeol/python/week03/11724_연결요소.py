b87e7e47636454b8464ce87d6d895b2c29c1cc9b
yylee0102 <yyiww112@gmail.com>
2025-05-17 22:18:15 +0900

    [BOJ] linked/실버5

--- yeol/python/week03/11724_연결요소.py
+++ yeol/python/week03/11724_연결요소.py
@@ -1,5 +1,9 @@
-n, m = map(int, input().split())  # 첫 줄: 정점 수 N, 간선 수 M
-A = [list(map(int, input().split())) for _ in range(M)]
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())  # 정점 수 N, 간선 수 M
+A = [list(map(int, input().split())) for _ in range(m)]  # ← 여기 고쳤어요!
 
 # dfs 함수
 def dfs(graph, v, visited):
@@ -9,15 +13,15 @@ def dfs(graph, v, visited):
             dfs(graph, i, visited)
 
 graph = [[] for _ in range(n+1)]
-for u,v in A:
-  graph[u].append(v)
-  graph[v].append(u)
+for u, v in A:
+    graph[u].append(v)
+    graph[v].append(u)
 
-count = 0 # 연결 노드의 수
 visited = [False] * (n+1)
-count = -1
-for i in range(0, n+1):
+count = 0
+for i in range(1, n+1):
     if not visited[i]:
         dfs(graph, i, visited)
         count += 1
+
 print(count)
