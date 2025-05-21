31d8f3e8879c05a891e11fb642a75e1eae6215d9
cheny <cheyeon001214@naver.com>
2025-05-21 10:39:22 +0900

    [BOJ] DFS와BFS/실버2

---
+++ codus/week02/ABCDE_13023.java
@@ -0,0 +1,18 @@
+package codus.week02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ABCDE_13023 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+    }
+}
